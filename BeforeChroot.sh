976d7bfab6dd25ace1397d9d4c278048a7892809
LittleControl <i@littlecontrol.cc>
2021-09-22 14:23:26 +0800

    pacstrap之前先选取速度较快的几个镜像

--- BeforeChroot.sh
+++ BeforeChroot.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 ls /sys/firmware/efi/efivars
 timedatectl set-ntp true
-pacstrap /mnt base linux linux-firmware linux-headers vim iwd man-db man-pages texinfo base-devel grub efibootmgr os-prober ntfs-3g
+# Shell的一些基本操作不是很会,只能暂时用这种基本笨的办法了
+echo "" >> /etc/xdg/reflector/reflector.conf
+echo "--save /etc/pacman.d/mirrorlist" >> /etc/xdg/reflector/reflector.conf
+echo "--protocol https,http" >> /etc/xdg/reflector/reflector.conf
+echo "--country China" >> /etc/xdg/reflector/reflector.conf
+echo "--latest 5" >> /etc/xdg/reflector/reflector.conf
+echo "--sort rate" >> /etc/xdg/reflector/reflector.conf
+systemctl start reflector.service
+pacstrap /mnt base linux linux-firmware linux-headers vim iwd man-db man-pages texinfo base-devel grub efibootmgr os-prober ntfs-3g bash-completion zsh zsh-completions
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
