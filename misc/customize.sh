ecf77a34ac34395da032c77e6792b78d61e0c7a6
LittleControl <i@littlecontrol.cc>
2021-09-23 14:50:21 +0800

    basic script

--- misc/customize.sh
+++ misc/customize.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
 # Grub Theme
+mkdir -p ~/Downloads && cd ~Downloads
+git clone https://github.com/vinceliuice/grub2-themes.git
+cd grub2-themes
+sudo chmod +x ./install.sh
+sudo ./install.sh -b -t tela
 
 # Font Config
 pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 # Basic Software
 
-pacman -S firefox tlp 
+yay -S firefox nodejs npm yarn visual-studio-code-bin lsof nss
+
+# oh-my-zsh
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# sound driver
+yay -S alsa-utils pulseaudio pulseaudio-alsa  pipewire pipewire-docs pipewire-alsa pipewire-pulse helvum 
