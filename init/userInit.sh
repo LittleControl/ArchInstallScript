f5ba9114a08445ca72a136f868ef6920972494cc
LittleControl <i@littlecontrol.cc>
2021-09-23 15:07:54 +0800

    change git conifg

--- init/userInit.sh
+++ init/userInit.sh
@@ -2,7 +2,7 @@
 
 git config --global user.name 'littlecontrol'
 git config --global user.email 'i@littlecontrol.me'
-git config --global user.autocrlf true
+git config --global user.autocrlf input
 cp /etc/zsh/zshrc ~/.zshrc
 mkdir ~/Downloads && cd ~/Downloads
 pacman -S go
