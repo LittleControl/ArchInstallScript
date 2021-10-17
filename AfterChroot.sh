a98f535239e3f0ee2f98a53ac64f918198d3d25a
littlecontrol <i@littlecontrol.me>
2021-10-17 22:16:02 +0800

    fix: sed修改grub配置文件参数修复 feat: 增加systemd-boot相关脚本

--- AfterChroot.sh
+++ AfterChroot.sh
@@ -10,12 +10,14 @@ echo "::1           Inspiron" >> /etc/hosts
 echo "127.0.1.1     Inspiron.localdomain  Inspiron" >> /etc/hosts
 sed -i 's/#COMPRESSION="zstd"/COMPRESSION="zstd/g"' /etc/mkinitcpio.conf
 mkinitcpio -P linux
+# GRUB
 sed -i '8a\GRUB_DISABLE_OS_PROBER=false' /etc/default/grub
-# echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
-sed -i '/^GRUB_DEFAULT/c\GRUB_DEFAULT=saved'
-sed -i '/#GRUB_SAVEDEFAULT/c\GRUB_SAVEDEFAULT=true'
+sed -i '/^GRUB_DEFAULT/c\GRUB_DEFAULT=saved' /etc/default/grub
+sed -i '/#GRUB_SAVEDEFAULT/c\GRUB_SAVEDEFAULT=true' /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+# Systemd-boot
+# bootctl install
 echo "please set your root password"
 passwd root
 echo "basic setting finished, now back to archiso"
