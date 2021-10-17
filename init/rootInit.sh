e912fd54b3ed19d23966311b7d2d7a02931a82aa
littlecontrol <i@littlecontrol.me>
2021-10-17 22:22:31 +0800

    fix: 优化对于reflector.conf的修改

--- init/rootInit.sh
+++ init/rootInit.sh
@@ -2,14 +2,12 @@
 timedatectl set-ntp true
 hwclock -w
 pacman -S reflector btrfs-progs terminus-font git
-setfont ter-124b 
+setfont ter-124b
 git config --system user.name 'littlecontrol'
 git config --system user.email 'i@littlecontrol.me'
 git config --system user.autocrlf input
 cp misc/.vimrc ~/
-echo "" > /etc/xdg/reflector/reflector.conf
-echo "--save /etc/pacman.d/mirrorlist" >> /etc/xdg/reflector/reflector.conf
-echo "--protocol https,http" >> /etc/xdg/reflector/reflector.conf
+echo "--save /etc/pacman.d/mirrorlist" > /etc/xdg/reflector/reflector.conf
 echo "--country China" >> /etc/xdg/reflector/reflector.conf
 echo "--latest 5" >> /etc/xdg/reflector/reflector.conf
 echo "--sort rate" >> /etc/xdg/reflector/reflector.conf
