3164fd2b443b68412a5a88cdba468bcefc0a0805
littlecontrol <i@littlecontrol.me>
2021-11-22 22:35:42 +0800

    fix: 优化脚本逻辑

--- BeforeChroot.sh
+++ BeforeChroot.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-FILE_DIR=`dirname $0`
-echo FILE_DIR
-ls /sys/firmware/efi/efivars
 echo "set time sync"
 timedatectl set-ntp true
 echo "--save /etc/pacman.d/mirrorlist" > /etc/xdg/reflector/reflector.conf
@@ -13,8 +10,5 @@ echo "--sort rate" >> /etc/xdg/reflector/reflector.conf
 echo "Selecting the fastest mirror, please wait..."
 systemctl start reflector.service
 echo "Mirror selection completed..."
-pacstrap /mnt base linux linux-firmware linux-headers vim iwd man-db man-pages texinfo base-devel bash-completion zsh git amd-ucode
+pacstrap /mnt base linux linux-firmware linux-headers neovim iwd man-db man-pages texinfo base-devel bash-completion zsh git amd-ucode btrfs-progs
 genfstab -U /mnt >> /mnt/etc/fstab
-cd FILE_DIR
-cp ./ArchInstallScript /mnt/root/
-arch-chroot /mnt
