0c565ec45c9a98301640b434ef7b292675530e46
littlecontrol <i@littlecontrol.me>
2021-12-19 01:06:50 +0800

    feat: 将grub和systemd-boot分为单独的文件,并加入systemd-boot 更新 hook

---
+++ bootloader/systemd-boot.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# systemd-boot
+
+bootctl install
+
+echo "default  arch.conf" > /boot/loader/loader.conf
+echo "timeout  3" >> /boot/loader/loader.conf
+echo "console-mode  max" >> /boot/loader/loader.conf
+
+echo "title   Arch Linux" > /boot/loader/entries/arch.conf
+echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch.conf
+echo "initrd    /amd-ucode.img" >> /boot/loader/entries/arch.conf
+echo "initrd    /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+## btrfs filesystem setting
+echo "options   root=\"LABEL=rootfs\"  rw rootflags=subvol=@" >> /boot/loader/entries/arch.conf
+
+echo "title   Arch Linux (fallback initramfs)" > /boot/loader/entries/arch-fallback.conf
+echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch-fallback.conf
+echo "initrd    /amd-ucode.img" >> /boot/loader/entries/arch-fallback.conf
+echo "initrd    /initramfs-linux-fallback.img" >> /boot/loader/entries/arch-fallback.conf
+echo "options   root=\"LABEL=rootfs\"  rw rootflags=subvol=@" >> /boot/loader/entries/arch-fallback.conf
+
+## pacman hooks
+echo "[Trigger]" > /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "Type = Package" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "Operation = Upgrade" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "Target = systemd" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "[Action]" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "Description = Updating systemd-boot" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "When = PostTransaction" >> /etc/pacman.d/hooks/100-systemd-boot.hook
+echo "Exec = /usr/bin/bootctl update" >> /etc/pacman.d/hooks/100-systemd-boot.hook
