78d4496928fddf02a756dd83f250bc2423e04da7
LittleControl <i@littlecontrol.cc>
2022-01-16 23:40:18 +0800

    feat: default options is setted for no btrfs filesystem

--- bootloader/systemd-boot.sh
+++ bootloader/systemd-boot.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
-# systemd-boot
+## systemd-boot install script
 
 bootctl install
 
 echo "default  arch.conf" > /boot/loader/loader.conf
-echo "timeout  3" >> /boot/loader/loader.conf
+echo "timeout  0" >> /boot/loader/loader.conf
 echo "console-mode  max" >> /boot/loader/loader.conf
 
 echo "title   Arch Linux" > /boot/loader/entries/arch.conf
 echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch.conf
 echo "initrd    /amd-ucode.img" >> /boot/loader/entries/arch.conf
 echo "initrd    /initramfs-linux.img" >> /boot/loader/entries/arch.conf
+echo "options   root=\"LABEL=rootfs\" rw" >> /boot/loader/entries/arch.conf
+
 ## btrfs filesystem setting
-echo "options   root=\"LABEL=rootfs\"  rw rootflags=subvol=@" >> /boot/loader/entries/arch.conf
+# echo "options   root=\"LABEL=rootfs\"  rw rootflags=subvol=@" >> /boot/loader/entries/arch.conf
 
 echo "title   Arch Linux (fallback initramfs)" > /boot/loader/entries/arch-fallback.conf
 echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch-fallback.conf
 echo "initrd    /amd-ucode.img" >> /boot/loader/entries/arch-fallback.conf
 echo "initrd    /initramfs-linux-fallback.img" >> /boot/loader/entries/arch-fallback.conf
-echo "options   root=\"LABEL=rootfs\"  rw rootflags=subvol=@" >> /boot/loader/entries/arch-fallback.conf
+echo "options   root=\"LABEL=rootfs\"  rw" >> /boot/loader/entries/arch-fallback.conf
+
+## btrfs filesystem setting
+# echo "options   root=\"LABEL=rootfs\"  rw rootflags=subvol=@" >> /boot/loader/entries/arch-fallback.conf
 
 ## pacman hooks
 echo "[Trigger]" > /etc/pacman.d/hooks/100-systemd-boot.hook
